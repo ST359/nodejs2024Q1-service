c38ee36e667f1f0a56f8a53f41bdc301da8d4af4
ST359 <135271126+ST359@users.noreply.github.com>
2024-03-10 04:21:20 +0300

    fix: move modules into upper directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, UserModule, ArtistModule, TrackModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
