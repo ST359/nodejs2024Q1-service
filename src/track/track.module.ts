a16e7c41cb34f23c69efb1f468d13d6d488eeb45
ST359 <135271126+ST359@users.noreply.github.com>
2024-03-11 01:34:33 +0300

    feat: create files in dirs according to docs

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TrackService } from './track.service';
+import { TrackController } from './track.controller';
 
-@Module({})
+@Module({
+  providers: [TrackService],
+  controllers: [TrackController]
+})
 export class TrackModule {}
